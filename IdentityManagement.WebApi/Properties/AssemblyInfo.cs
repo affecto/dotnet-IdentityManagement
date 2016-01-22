8def00e0bc93e31c1de95daa074cea44596bb70a
Ville Ruuskanen <ville.ruuskanen@affecto.com>
2016-01-22 15:09:22 +0200

    Fixed webapi version number

--- IdentityManagement.WebApi/Properties/AssemblyInfo.cs
+++ IdentityManagement.WebApi/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@
 [assembly: AssemblyProduct("Affecto.IdentityManagement")]
 [assembly: AssemblyCompany("Affecto")]
 
-[assembly: AssemblyVersion("8.0.0.0")]
-[assembly: AssemblyFileVersion("8.0.0.0")]
+[assembly: AssemblyVersion("9.0.0.0")]
+[assembly: AssemblyFileVersion("9.0.0.0")]
 
 // This version is used by NuGet:
-[assembly: AssemblyInformationalVersion("8.0.0")]
+[assembly: AssemblyInformationalVersion("9.0.0-prerelease01")]
