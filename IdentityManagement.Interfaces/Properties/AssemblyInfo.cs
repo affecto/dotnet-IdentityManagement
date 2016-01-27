6e96ad88682360b0f02692a7aa718f4d15842817
Ville Ruuskanen <ville.ruuskanen@affecto.com>
2016-01-27 12:21:32 +0200

    Nuget version to 9.0.0

--- IdentityManagement.Interfaces/Properties/AssemblyInfo.cs
+++ IdentityManagement.Interfaces/Properties/AssemblyInfo.cs
@@ -9,4 +9,4 @@
 [assembly: AssemblyFileVersion("9.0.0.0")]
 
 // This version is used by NuGet:
-[assembly: AssemblyInformationalVersion("9.0.0-prerelease01")]
+[assembly: AssemblyInformationalVersion("9.0.0")]
