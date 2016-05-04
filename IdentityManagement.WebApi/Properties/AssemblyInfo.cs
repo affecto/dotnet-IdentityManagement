38d2f73f4eb866c89e61267c86cff1e0e1da51ab
Ville Henriksson <teettari@hotmail.com>
2016-05-04 10:09:30 +0300

    Raise to next prerelease version numbers and add release notes.

--- IdentityManagement.WebApi/Properties/AssemblyInfo.cs
+++ IdentityManagement.WebApi/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@
 [assembly: AssemblyProduct("Affecto.IdentityManagement")]
 [assembly: AssemblyCompany("Affecto")]
 
-[assembly: AssemblyVersion("9.0.1.0")]
-[assembly: AssemblyFileVersion("9.0.1.0")]
+[assembly: AssemblyVersion("9.0.2.0")]
+[assembly: AssemblyFileVersion("9.0.2.0")]
 
 // This version is used by NuGet:
-[assembly: AssemblyInformationalVersion("9.0.1")]
+[assembly: AssemblyInformationalVersion("9.0.2-prerelease01")]
